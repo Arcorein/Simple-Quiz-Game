e78f6b4bd0a514f52599289cd886cdfdb5735857
Rafi <ahmadrafiin467@gmail.com>
2025-06-24 22:18:06 +0800

    Add score display and player feedback to quiz game: implement score tracking, display player name and score upon quiz completion, and enhance UI styling

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let playerName;
 let randomIndexPicker;
 const questionObjectList = []
 
-
 let questionDisplay;
 let choiceDisplay;
 let indexPicker = 0;
@@ -32,9 +31,13 @@ function QuestionTemplate(question,choice,answer){
     this.question = question;
     this.choice = choice;
     this.answer = answer;
-    
-    
+       
 }
+
+const player = {
+    name: localStorage.getItem("playerName"),
+    score: 0
+};
 
 const question1 = new QuestionTemplate(questionList[0],answerList[0],answerList[0][3]);
 const question2 = new QuestionTemplate(questionList[1],answerList[1],answerList[1][0]);
@@ -43,16 +46,6 @@ const question4 = new QuestionTemplate(questionList[3],answerList[3],answerList[
 const question5 = new QuestionTemplate(questionList[4],answerList[4],answerList[4][1]);
 
 questionObjectList.push(question1,question2,question3,question4,question5)
-
-
-
-function getQueueQuestion(){
-    let x;
-    let question;
-    for(x in questionObjectList){
-        
-    }
-}
 
 function getFirstQuestionDisplay(){
 
@@ -65,18 +58,17 @@ function getFirstQuestionDisplay(){
     document.getElementById("choice2").textContent = choiceDisplay[1];
     document.getElementById("choice3").textContent = choiceDisplay[2];
     document.getElementById("choice4").textContent = choiceDisplay[3];
-    
 }
 
 function startButton(){
-    playerName = document.getElementById("playerName").value
+    playerName = document.getElementById("playerName").value;
+    localStorage.setItem("playerName",playerName);
     if(playerName){
         let button = document.getElementById("start-button").onclick;
         button = window.location.href="quiz.html";   
     } else {
         document.getElementById("alert-result").textContent = "Input Player Name First!"
     }
-    
 }
 
 function answerButton(answerChoice){
@@ -84,20 +76,13 @@ function answerButton(answerChoice){
     let getAnswer = questionObjectList[indexPicker].answer;
     choiceDisplay = questionObjectList[indexPicker].choice;
     
-
-    
-    
-    
-    
-    if(getAnswer == answerChoice){
-        
-        for(x in choiceDisplay){
-            
+    if(getAnswer == answerChoice){ 
+        player.score+=1;  
+        for(x in choiceDisplay){   
             if(getAnswer === choiceDisplay[x]){
                 console.log("ini answerChoice = "+answerChoice)
                 console.log("Ini x = "+x)
-                console.log(choiceDisplay[x])
-                
+                console.log(choiceDisplay[x])  
                 getButtonElement[0] = document.getElementById("choice1")
                 getButtonElement[1] = document.getElementById("choice2")
                 getButtonElement[2] = document.getElementById("choice3")
@@ -106,60 +91,68 @@ function answerButton(answerChoice){
                 getButtonElement[x].style.backgroundColor = "green"
             } else {
                 console.log(choiceDisplay[x])
-              
+            
                 getButtonElement[0] = document.getElementById("choice1")
                 getButtonElement[1] = document.getElementById("choice2")
                 getButtonElement[2] = document.getElementById("choice3")
                 getButtonElement[3] = document.getElementById("choice4")
                 console.log(false)
-                getButtonElement[x].style.backgroundColor = "red"
-                
+                getButtonElement[x].style.backgroundColor = "red"  
             }
         }
     } else {
-        for(x in choiceDisplay){
-            
+        for(x in choiceDisplay){   
             if(getAnswer === choiceDisplay[x]){
-                
                 getButtonElement[0] = document.getElementById("choice1")
                 getButtonElement[1] = document.getElementById("choice2")
                 getButtonElement[2] = document.getElementById("choice3")
-                getButtonElement[3] = document.getElementById("choice4")
-                
+                getButtonElement[3] = document.getElementById("choice4")   
                 getButtonElement[x].style.backgroundColor = "green"
             } else {
                 console.log(choiceDisplay[x])
-              
+
                 getButtonElement[0] = document.getElementById("choice1")
                 getButtonElement[1] = document.getElementById("choice2")
                 getButtonElement[2] = document.getElementById("choice3")
                 getButtonElement[3] = document.getElementById("choice4")
-                getButtonElement[x].style.backgroundColor = "red"
-                
+                getButtonElement[x].style.backgroundColor = "red"  
             }
-        }
-        
+        }  
     }
-    document.getElementById("next-button").innerHTML = '<button onclick="nextButton()" id="nextButton">Next Question</button>';
-    
-
+    if(indexPicker <= questionObjectList.length-2){
+        document.getElementById("next-button").innerHTML = '<button onclick="nextButton()" id="nextButton">Next Question</button>';  
+    } else {
+        document.getElementById("next-button").innerHTML = '<button onclick="nextButton()" id="nextButton">Finish Quiz</button>';
+    }
 }
 
 function nextButton(){
-    for(x in getButtonElement){
-        getButtonElement[x].style.backgroundColor = "";
-    }
     indexPicker+=1;
-    questionDisplay = questionObjectList[indexPicker].question;
-    choiceDisplay = questionObjectList[indexPicker].choice;
+    if(indexPicker <= questionObjectList.length-1){
+        for(x in getButtonElement){
+            getButtonElement[x].style.backgroundColor = "";
+        }
+        questionDisplay = questionObjectList[indexPicker].question;
+        choiceDisplay = questionObjectList[indexPicker].choice;
 
-    document.getElementById("question-display").textContent = questionDisplay;
-    document.getElementById("choice1").textContent = choiceDisplay[0];
-    document.getElementById("choice2").textContent = choiceDisplay[1];
-    document.getElementById("choice3").textContent = choiceDisplay[2];
-    document.getElementById("choice4").textContent = choiceDisplay[3];
-    
-    document.getElementById("nextButton").remove();
-    document.getElementsByTagName("body").style.transition = "all 0.2s";
-    
+        document.getElementById("question-display").textContent = questionDisplay;
+        document.getElementById("choice1").textContent = choiceDisplay[0];
+        document.getElementById("choice2").textContent = choiceDisplay[1];
+        document.getElementById("choice3").textContent = choiceDisplay[2];
+        document.getElementById("choice4").textContent = choiceDisplay[3];
+        
+        document.getElementById("nextButton").remove();
+        
+    }else{
+        displayScore()
+    }
+}
+
+function displayScore(){
+    let playerScore = (player.score/questionObjectList.length)*100;
+    console.log("terpanggil")
+    console.log(player.name);
+    document.getElementById("display-score").style.display = "block"
+    document.getElementById("player-name").innerHTML = player.name;
+    document.getElementById("player-score").innerHTML =playerScore;
 }
